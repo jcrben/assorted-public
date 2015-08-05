b55e0c295baf85b00cfda7f369c13259d2706fcf
Ben Creasy <contact@bencreasy.com>
2015-08-04 23:14:00 -0700

    Add simple challenge getSum.js

---
+++ problems/getSum.js
@@ -0,0 +1,23 @@
+// the other questions were about cloning (examples were primitives)
+// and an easy one using console logs and assigning values
+
+function getSum(endpoints) {
+    var sum = 0;
+    var returns = 0;
+    // ...
+    
+    endpoints.forEach(function(val, idx, arr) {
+      $.get(val, function(d) {
+      })
+      .then(function(d) {
+        sum += logins;
+        returns += 1;
+        if (returns === endpoints.length) {
+          return sum;
+        }
+    });
+}
+ 
+$.get('/serviceA?user=foo', function(d) {
+    // ...
+});
