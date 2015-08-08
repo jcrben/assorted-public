d29c42e94cf1a1afac86b6eb21dec134ad7194a5
Ben Creasy <contact@bencreasy.com>
2015-08-07 23:17:30 -0700

    Add d3 experiments

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -1,33 +1,58 @@
 
 
-var data = [4, 8, 15, 16, 23, 42];
+// var data = [30, 4, 8, 15, 16, 17, 23, 42];
 
-var width = 420;
-var height = 20;
+var data = [
+    { "2011": [1000, 2000, 500] },
+    { "2012": [1000, 2000, 500] },
+    { "2013": [1000, 2000, 500] }
+];
+var width = 300;
+var columnHeight = 20;
+var height = 500;
 
-var xAxis = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
-
+var y = d3.scale.linear()
+    .domain([0, 2000])
+    .range([0, height]);
+// debugger;
 var chart = d3.select('.chart')
     .attr('width', width)
-    .attr('height', barHeight * data.length);
+    .attr('height', height);
 
+var barWidth = width/data.length;
 
 var bar = chart.selectAll('g')
     .data(data)
   .enter().append('g')
-    .attr('transform', function (d, i) { return 'translate(0,' + i * barHeight + ")"; });
+    // .attr('transform', function (d, i) { return 'translate(0,' + i * columnHeight + ")"; });
 
 bar.append('rect')
-    .attr('width', xAxis)
-    .attr('height', barHeight - 1);
+    .attr("y", function(d) { 
+        var key = Object.keys(d)[0];
+        return height - y(d[key][0]); 
+        })
+    .attr("x", function(d, i) { return i*barWidth})
+    .attr("height", function(d) { 
+        var key = Object.keys(d)[0];
+        return y(d[key][0])})
+    .attr("width", barWidth)
+    .attr('fill', "blue");
 
-bar.append('text')
-    .attr('x', function(d) { return xAxis(d) - 3; })
-    .attr('y', barHeight /2)
-    .attr('dy', '.35em')
-    .text(function(d) { return d; });
+bar.append('rect')
+    .attr("y", function(d) { 
+        var key = Object.keys(d)[0];
+        return height - y(d[key][1]) - y(d[key][0]); })
+    .attr("x", function(d, i) { return i*barWidth})
+    .attr("height", function(d) { 
+        var key = Object.keys(d)[0];
+        return y(d[key][1])})
+    .attr("width", barWidth)
+    .attr('fill', "green");
+// bar.append('text')
+//     .attr('x', function(d) { return y(d) - 3; })
+//     .attr('y', columnHeight /2)
+//     .attr('dy', '.35em')
+//     .text(function(d) { return d; });
 
 
 // d3.select('body')
